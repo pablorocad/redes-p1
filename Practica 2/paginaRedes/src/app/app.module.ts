ea1aafd7b41d94d8110ab8c5357ef3d4914a1e8b
Luis Arana <luarana631@gmail.com>
2021-03-30 21:05:33 -0600

    Update 50% página web

--- Practica 2/paginaRedes/src/app/app.module.ts
+++ Practica 2/paginaRedes/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BooksComponent } from './pages/books/books.component';
+import { AddBookComponent } from './pages/add-book/add-book.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { BookComponent } from './pages/book/book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BooksComponent,
+    AddBookComponent,
+    FooterComponent,
+    NavbarComponent,
+    ContactoComponent,
+    BookComponent
   ],
   imports: [
     BrowserModule,
